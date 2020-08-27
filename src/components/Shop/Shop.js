c23b8013cd5fe14e0079f0985eff145b2b510ee5
Nur A Alam <nuraalam376@gmail.com>
2020-08-27 09:24:08 +0600

    separated courses fake data

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,38 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Course from "../Course/Course";
 import Cart from "../Cart/Cart";
+import CoursesData from "../../fakeData/courses";
 
 const Shop = () => {
 	const [courses, setCourses] = useState([]);
 	const [cart, setCart] = useState([]);
 
 	useEffect(() => {
-		const allCourses = [
-			{
-				id: 1,
-				title: "React.js",
-				price: 1000,
-				instructor: "Ryan Dahl",
-				duration: 50,
-				image: "https://probella.com/wp-content/uploads/2018/03/React-JS.png",
-			},
-			{
-				id: 2,
-				title: "Node.js",
-				price: 2000,
-				instructor: "Brad Traversy",
-				duration: 30,
-				image: "https://probella.com/wp-content/uploads/2018/03/React-JS.png",
-			},
-			{
-				id: 3,
-				title: "Python",
-				price: 5000,
-				instructor: "Maximilian",
-				duration: 70,
-				image: "https://probella.com/wp-content/uploads/2018/03/React-JS.png",
-			},
-		];
+		// Set the courses fake data
+		const allCourses = CoursesData;
 		setCourses(allCourses);
 	}, []);
 
---
+++ src/fakeData/courses.js
@@ -0,0 +1,28 @@
+const allCourses = [
+	{
+		id: 1,
+		title: "React.js",
+		price: 1000,
+		instructor: "Ryan Dahl",
+		duration: 50,
+		image: "https://probella.com/wp-content/uploads/2018/03/React-JS.png",
+	},
+	{
+		id: 2,
+		title: "Node.js",
+		price: 2000,
+		instructor: "Brad Traversy",
+		duration: 30,
+		image: "https://probella.com/wp-content/uploads/2018/03/React-JS.png",
+	},
+	{
+		id: 3,
+		title: "Python",
+		price: 5000,
+		instructor: "Maximilian",
+		duration: 70,
+		image: "https://probella.com/wp-content/uploads/2018/03/React-JS.png",
+	},
+];
+
+export default allCourses;
